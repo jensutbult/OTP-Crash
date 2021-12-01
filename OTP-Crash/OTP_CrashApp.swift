0753db7aabbcd44be89f0cb2866649ba6f9020fb
Jens Utbult <jens.utbult@yubico.com>
2021-12-01 11:49:39 +0100

    Initial Commit

---
+++ OTP-Crash/OTP_CrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTP_CrashApp.swift
+//  OTP-Crash
+//
+//  Created by Jens Utbult on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct OTP_CrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
